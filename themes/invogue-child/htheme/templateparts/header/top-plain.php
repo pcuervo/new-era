8b048a54685f03182c9747311146f0701fe958c7
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-08-23 14:33:06 -0500

    eliminado codigo de header default de categorias

--- themes/invogue-child/htheme/templateparts/header/top-plain.php
+++ themes/invogue-child/htheme/templateparts/header/top-plain.php
@@ -12,39 +12,6 @@
 				<div class="[ header-category ]">
 					<h1><?php the_title(); ?></h1>
 				</div>
-				<div class="htheme_title_container" data-title-type="default">
-					<div class="htheme_title">
-						<h1>
-							<?php if(isset($_GET['get_title'])){ ?>
-								<?php echo esc_html($_GET['get_title']); ?>
-							<?php } else { ?>
-								<?php
-								if($htheme_meta_title != ''){
-									echo esc_html($htheme_meta_title);
-								} else {
-									esc_html(the_title());
-								}
-								?>
-							<?php } ?>
-						</h1>
-					</div>
-					<?php if($htheme_meta_devider != '' && $htheme_meta_devider != 'none'){ ?>
-						<div class="htheme_svg_holder">
-							<?php echo wp_remote_fopen(get_template_directory_uri().'/htheme/assets/svg/htheme_'.$htheme_meta_devider.'.svg'); ?>
-						</div>
-					<?php } ?>
-					<?php if(class_exists( 'WooCommerce' ) && is_shop() || class_exists( 'WooCommerce' ) && is_product_category() || class_exists( 'WooCommerce' ) && is_product_tag()){ ?>
-					<!-- DO NOTHING -->
-					<?php } else if($htheme_meta_excerpt != '') { ?>
-						<div class="htheme_sub_title htheme_h1_sub">
-							<?php echo esc_html($htheme_meta_excerpt); ?>
-						</div>
-					<?php } else if($htheme_meta_excerpt == '' && $post->post_excerpt != '') { ?>
-						<div class="htheme_sub_title htheme_h1_sub">
-							<?php echo esc_html($post->post_excerpt); ?>
-						</div>
-					<?php } ?>
-				</div>
 			</div>
 		</div>
 	</div>
