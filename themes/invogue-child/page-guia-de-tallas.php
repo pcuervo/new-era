5a0f47e095baa9a27dde572d55c9a520fbcd6d1a
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-08-30 19:01:32 -0500

    page guia de talla

---
+++ themes/invogue-child/page-guia-de-tallas.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+<div class="htheme_content_holder">
+	<div class="htheme_row_margin_top_bottom htheme_vc_row_contained">
+		<div class="wpb_column vc_column_container vc_col-sm-12">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper"><!-- ROW -->
+					<div class="htheme_row">
+						<div class="htheme_container">
+							<div class="htheme_inner_col"><!-- TITLE DOUBLE TOP BOTTOM -->
+								<div class="htheme_title_container" data-title-type="top_bottom">
+									<div class="htheme_title">
+										<h2>Guía de tallas</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div><!-- ROW -->
+					<div class="wpb_text_column wpb_content_element ">
+						<div class="wpb_wrapper">
+							<div class="htheme_inner_col htheme_default_content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="htheme_row_margin_top_bottom htheme_vc_row_contained">
+		<div class="wpb_column vc_column_container vc_col-sm-12">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper">
+					<div class="wpb_text_column wpb_content_element ">
+						<div class="wpb_wrapper">
+							<div class="htheme_inner_col htheme_default_content">
+								<a href="http://localhost:8888/new-era/knits/"><h3>59FIFTY &amp; 19TWENTY</h3></a>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
