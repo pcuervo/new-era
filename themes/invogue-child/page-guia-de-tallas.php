1f9121d38ae78df85cd944f93436b43fd2c99fef
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-08-30 19:17:46 -0500

    backgroun colors, gray y primary

--- themes/invogue-child/page-guia-de-tallas.php
+++ themes/invogue-child/page-guia-de-tallas.php
@@ -27,7 +27,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="htheme_row_margin_top_bottom htheme_vc_row_contained">
+	<div class="htheme_vc_row_contained">
 		<div class="wpb_column vc_column_container vc_col-sm-12">
 			<div class="vc_column-inner ">
 				<div class="wpb_wrapper">
@@ -35,7 +35,92 @@
 						<div class="wpb_wrapper">
 							<div class="htheme_inner_col htheme_default_content">
 								<a href="http://localhost:8888/new-era/knits/"><h3>59FIFTY &amp; 19TWENTY</h3></a>
-
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="htheme_vc_row_contained">
+		<div class="wpb_column vc_column_container vc_col-sm-6">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper">
+					<div class="wpb_text_column wpb_content_element ">
+						<div class="wpb_wrapper">
+							<div class="htheme_inner_col htheme_default_content">
+								<table class="[ text-center ]">
+									<tr class="[ background-color--primary ]">
+										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
+										<th class="[ paddin-top-bottom ]">Talla gorra</th>
+										<th class="[ paddin-top-bottom ]">Edad</th>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>48.2</td>
+										<td>6</td>
+										<td>infantil</td>
+									</tr>
+									<tr class="[ background-color--gray--lighter ]">
+										<td>51.1</td>
+										<td>6 3/8</td>
+										<td>Niño pequeño</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>52</td>
+										<td>6 1/2</td>
+										<td>Niño</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>53</td>
+										<td>6 3/4</td>
+									</tr>
+									<tr class="[ background-color--gray--lighter ]">
+										<td>53.9</td>
+										<td>6 7/8</td>
+										<td>Joven</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>54.9</td>
+										<td>7</td>
+										<td>Adulto</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>55.8</td>
+										<td>7 1/8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>56.8</td>
+										<td>7 1/4</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>57.7</td>
+										<td>7 3/8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>58.7</td>
+										<td>7 1/2</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>59.6</td>
+										<td>7 5/8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>60.6</td>
+										<td>7 3/4</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>61.5</td>
+										<td>7 7/8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>62.5</td>
+										<td>7 7/8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>63.5</td>
+										<td>8</td>
+									</tr>
+								</table>
 							</div>
 						</div>
 					</div>
