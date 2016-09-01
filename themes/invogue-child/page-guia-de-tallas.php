be48af8942f87066df32d2a98167d9e45e71f773
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-09-01 17:18:13 -0500

    tablas guia sin borde y correcciones de estilo y datos

--- themes/invogue-child/page-guia-de-tallas.php
+++ themes/invogue-child/page-guia-de-tallas.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div class="htheme_content_holder">
+<div class="htheme_content_holder [ content-guide-size ]">
 	<div class="htheme_row_margin_top_bottom htheme_vc_row_contained">
 		<div class="wpb_column vc_column_container vc_col-sm-12">
 			<div class="vc_column-inner ">
@@ -35,7 +35,7 @@
 						<div class="wpb_wrapper">
 							<div class="htheme_inner_col htheme_default_content">
 								<a href="http://localhost:8888/new-era/knits/"><h4>59FIFTY &amp; 19TWENTY</h4></a>
-								<table class="[ text-center ]">
+								<table class="[ text-center ]" border="0" cellspacing="0" cellpadding="0">
 									<tr class="[ background-color--primary ]">
 										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
 										<th class="[ paddin-top-bottom ]">Talla gorra</th>
@@ -58,45 +58,45 @@
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>53</td>
-										<td>6 3/4</td>
+										<td>6 5/8</td>
 									</tr>
 									<tr class="[ background-color--gray--medium ]">
 										<td>53.9</td>
-										<td>6 7/8</td>
-										<td>Youth</td>
+										<td>6 3/4</td>
+										<td rowspan="2">Youth</td>
 									</tr>
-									<tr class="[ background-color--gray--light ]">
+									<tr class="[ background-color--gray--medium ]">
 										<td>54.9</td>
-										<td>7</td>
-										<td rowspan="12">Adult</td>
+										<td>6 7/8</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>55.8</td>
-										<td>7 1/8</td>
+										<td>7</td>
+										<td rowspan="11">Adult</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>56.8</td>
-										<td>7 1/4</td>
+										<td>7 1/8</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>57.7</td>
-										<td>7 3/8</td>
+										<td>7 1/4</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>58.7</td>
-										<td>7 1/2</td>
+										<td>7 3/8</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>59.6</td>
-										<td>7 5/8</td>
+										<td>7 1/2</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>60.6</td>
-										<td>7 3/4</td>
+										<td>7 5/8</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>61.5</td>
-										<td>7 7/8</td>
+										<td>7 3/4</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>62.5</td>
@@ -120,7 +120,7 @@
 						<div class="wpb_wrapper">
 							<div class="htheme_inner_col htheme_default_content">
 								<a href="http://localhost:8888/new-era/knits/"><h4>39THIRTY</h4></a>
-								<table class="[ text-center ]">
+								<table class="[ text-center ]" border="0" cellspacing="0" cellpadding="0">
 									<tr class="[ background-color--primary ]">
 										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
 										<th class="[ paddin-top-bottom ]" colspan="2">Talla gorra</th>
@@ -183,7 +183,7 @@
 						<div class="wpb_wrapper">
 							<div class="htheme_inner_col htheme_default_content">
 								<a href="http://localhost:8888/new-era/knits/"><h4>9FIFTY</h4></a>
-								<table class="[ text-center ]">
+								<table class="[ text-center ]" border="0" cellspacing="0" cellpadding="0">
 									<tr class="[ background-color--primary ]">
 										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
 										<th class="[ paddin-top-bottom ]" colspan="3">Talla gorra</th>
@@ -232,7 +232,7 @@
 						<div class="wpb_wrapper">
 							<div class="htheme_inner_col htheme_default_content">
 								<a href="http://localhost:8888/new-era/knits/"><h4>9FORTY & 9TWENTY</h4></a>
-								<table class="[ text-center ]">
+								<table class="[ text-center ]" border="0" cellspacing="0" cellpadding="0">
 									<tr class="[ background-color--primary ]">
 										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
 										<th class="[ paddin-top-bottom ]" colspan="3">Talla gorra</th>
@@ -273,7 +273,7 @@
 						<div class="wpb_wrapper">
 							<div class="htheme_inner_col htheme_default_content">
 								<a href="http://localhost:8888/new-era/knits/"><h4>BUCKET</h4></a>
-								<table class="[ text-center ]">
+								<table class="[ text-center ]" border="0" cellspacing="0" cellpadding="0">
 									<tr class="[ background-color--primary ]">
 										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
 										<th class="[ paddin-top-bottom ]" colspan="3">Talla gorra</th>
