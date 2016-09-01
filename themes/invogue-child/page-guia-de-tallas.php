28f2a49042e4f390af00d30b4e04e2a55cc80d26
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-09-01 17:02:06 -0500

    tablas guia aún con bordes entre lineas

--- themes/invogue-child/page-guia-de-tallas.php
+++ themes/invogue-child/page-guia-de-tallas.php
@@ -28,27 +28,13 @@
 		</div>
 	</div>
 	<div class="htheme_vc_row_contained">
-		<div class="wpb_column vc_column_container vc_col-sm-12">
-			<div class="vc_column-inner ">
-				<div class="wpb_wrapper">
-					<div class="wpb_text_column wpb_content_element ">
-						<div class="wpb_wrapper">
-							<div class="htheme_inner_col htheme_default_content">
-								<a href="http://localhost:8888/new-era/knits/"><h3>59FIFTY &amp; 19TWENTY</h3></a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="htheme_vc_row_contained">
 		<div class="wpb_column vc_column_container vc_col-sm-6">
 			<div class="vc_column-inner ">
 				<div class="wpb_wrapper">
 					<div class="wpb_text_column wpb_content_element ">
 						<div class="wpb_wrapper">
 							<div class="htheme_inner_col htheme_default_content">
+								<a href="http://localhost:8888/new-era/knits/"><h4>59FIFTY &amp; 19TWENTY</h4></a>
 								<table class="[ text-center ]">
 									<tr class="[ background-color--primary ]">
 										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
@@ -58,17 +44,17 @@
 									<tr class="[ background-color--gray--light ]">
 										<td>48.2</td>
 										<td>6</td>
-										<td>infantil</td>
+										<td>Infant</td>
 									</tr>
 									<tr class="[ background-color--gray--medium ]">
 										<td>51.1</td>
 										<td>6 3/8</td>
-										<td>Niño pequeño</td>
+										<td>Toddler</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>52</td>
 										<td>6 1/2</td>
-										<td>Niño</td>
+										<td rowspan="2">Child</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>53</td>
@@ -77,12 +63,12 @@
 									<tr class="[ background-color--gray--medium ]">
 										<td>53.9</td>
 										<td>6 7/8</td>
-										<td>Joven</td>
+										<td>Youth</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>54.9</td>
 										<td>7</td>
-										<td>Adulto</td>
+										<td rowspan="12">Adult</td>
 									</tr>
 									<tr class="[ background-color--gray--light ]">
 										<td>55.8</td>
@@ -119,6 +105,214 @@
 									<tr class="[ background-color--gray--light ]">
 										<td>63.5</td>
 										<td>8</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="wpb_column vc_column_container vc_col-sm-6">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper">
+					<div class="wpb_text_column wpb_content_element ">
+						<div class="wpb_wrapper">
+							<div class="htheme_inner_col htheme_default_content">
+								<a href="http://localhost:8888/new-era/knits/"><h4>39THIRTY</h4></a>
+								<table class="[ text-center ]">
+									<tr class="[ background-color--primary ]">
+										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
+										<th class="[ paddin-top-bottom ]" colspan="2">Talla gorra</th>
+									</tr>
+									<tr>
+										<td class="[ background-color--gray--light ]">53.9</td>
+										<td class="[ background-color--gray--light ]">Extra Chica - Chica</td>
+									</tr>
+									<tr>
+										<td class="[ background-color--gray--dark ]">54.9</td>
+										<td class="[ background-color--gray--dark ]"></td>
+										<td class="[ background-color--gray--medium ]" colspan="2" rowspan="4">Chica - Mediana</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td class="[ background-color--gray--medium ]">55.8</td>
+										<td class="[ background-color--gray--medium ]" rowspan="2"></td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td class="[ background-color--gray--medium ]">56.8</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td  class="[ background-color--gray--dark ]">57.7</td>
+										<td class="[ background-color--gray--dark ]"></td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>58.7</td>
+										<td rowspan="2">Mediana - Grande</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>59.6</td>
+									</tr>
+									<tr>
+										<td class="[ background-color--gray--dark ]">60.6</td>
+										<td class="[ background-color--gray--dark ]"></td>
+										<td class="[ background-color--gray--medium ]" colspan="2" rowspan="4">Grande - Extra grande</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>61.5</td>
+										<td class="[ background-color--gray--medium ]" rowspan="3"></td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>62.5</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>63.5</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="htheme_vc_row_contained">
+		<div class="wpb_column vc_column_container vc_col-sm-6">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper">
+					<div class="wpb_text_column wpb_content_element ">
+						<div class="wpb_wrapper">
+							<div class="htheme_inner_col htheme_default_content">
+								<a href="http://localhost:8888/new-era/knits/"><h4>9FIFTY</h4></a>
+								<table class="[ text-center ]">
+									<tr class="[ background-color--primary ]">
+										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
+										<th class="[ paddin-top-bottom ]" colspan="3">Talla gorra</th>
+									</tr>
+									<tr>
+										<td class="[ background-color--gray--light ]">54.9</td>
+										<td class="[ background-color--gray--light ]" rowspan="6">Chica - Mediana</td>
+										<td class="[ background-color--gray--dark ]" rowspan="2"></td>
+										<td class="[ background-color--gray--dark ]" rowspan="8">Unitalla</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>55.8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>56.8</td>
+										<td class="[ background-color--gray--medium ]" rowspan="6">Mediana - Grande</td>
+									</tr>
+									<tr>
+										<td class="[ background-color--gray--light ]">57.7</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>58.7</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>59.6</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>60.6</td>
+										<td></td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>61.5</td>
+										<td></td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="wpb_column vc_column_container vc_col-sm-3">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper">
+					<div class="wpb_text_column wpb_content_element ">
+						<div class="wpb_wrapper">
+							<div class="htheme_inner_col htheme_default_content">
+								<a href="http://localhost:8888/new-era/knits/"><h4>9FORTY & 9TWENTY</h4></a>
+								<table class="[ text-center ]">
+									<tr class="[ background-color--primary ]">
+										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
+										<th class="[ paddin-top-bottom ]" colspan="3">Talla gorra</th>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>55.8</td>
+										<td rowspan="9">Unitalla</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>56.8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>57.7</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>58.7</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>59.6</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>60.6</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>61.5</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="wpb_column vc_column_container vc_col-sm-3">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper">
+					<div class="wpb_text_column wpb_content_element ">
+						<div class="wpb_wrapper">
+							<div class="htheme_inner_col htheme_default_content">
+								<a href="http://localhost:8888/new-era/knits/"><h4>BUCKET</h4></a>
+								<table class="[ text-center ]">
+									<tr class="[ background-color--primary ]">
+										<th class="[ paddin-top-bottom ]">Circunferencia (cm)</th>
+										<th class="[ paddin-top-bottom ]" colspan="3">Talla gorra</th>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>54.9</td>
+										<td rowspan="3">Extra CH o CH/M</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>55.8</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>56.8</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>57.7</td>
+										<td>CH</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>58.7</td>
+										<td>M O M/G</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>59.6</td>
+										<td>G</td>
+									</tr>
+									<tr class="[ background-color--gray--light ]">
+										<td>60.6</td>
+										<td>Extra G o G/Extra G</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>61.5</td>
+										<td rowspan="3">Extra Extra G</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>62.5</td>
+									</tr>
+									<tr class="[ background-color--gray--medium ]">
+										<td>63.5</td>
 									</tr>
 								</table>
 							</div>
--- themes/invogue/htheme/classes/class.get.woo.php
+++ themes/invogue/htheme/classes/class.get.woo.php
@@ -549,15 +549,15 @@ class htheme_getwoo{
 				}
 				#IF PRODUCT IS OLDER THAN 20 DAYS
 				if ( has_term( 'exclusivo-online', 'product_cat' ) ) {
-					$html .= '<div class="htheme_product_list_new htheme_product_list_exclusivo-online">'.esc_html__('Exclusivo online','invogue').'</div>';
+					$html .= '<div class="htheme_product_list_new htheme_product_list_exclusivo-online"><div></div>'.esc_html__('Exclusivo online','invogue').'</div>';
 				}
 				#IF PRODUCT IS Más vendido
 				if ( has_term( 'mas-vendido', 'product_cat' ) ) {
-					$html .= '<div class="htheme_product_list_new htheme_product_list_mas-vendido">'.esc_html__('Más vendido','invogue').'</div>';
+					$html .= '<div class="htheme_product_list_new htheme_product_list_mas-vendido"><div></div>'.esc_html__('Más vendido','invogue').'</div>';
 				}
 				#IF PRODUCT IS Edición limitada
 				if ( has_term( 'edicion-limitada', 'product_cat' ) ) {
-					$html .= '<div class="htheme_product_list_new htheme_product_list_adicion-limitada">'.esc_html__('Edición limitada','invogue').'</div>';
+					$html .= '<div class="htheme_product_list_new htheme_product_list_adicion-limitada"><div></div>'.esc_html__('Edición limitada','invogue').'</div>';
 				}
 
 				#OPTIONS - WISHLIST, ADD, PREVIEW
