9256ac9c37088bba4a6c7add36d881273ec61cfd
Arisbeth <aris@pcuervo.com>
2016-09-05 12:51:00 -0500

    iframe form

--- themes/invogue-child/page-newsletter.php
+++ themes/invogue-child/page-newsletter.php
@@ -6,7 +6,7 @@
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/htheme/assets/images/lightbox/blog-new-era-pantallas.png" alt="nuevo blog New Era" class="computadora"> 
 			</div>
 			<div class="lightbox-container">
-					<iframe id="destination-frame" src="<?php echo get_stylesheet_directory_uri(); ?>/lightbox/formulario-lightbox-sitio.html"></iframe>
+					<iframe id="destination-frame" src="<?php echo get_stylesheet_directory_uri(); ?>/newsletter-iframe/formulario-lightbox-sitio.html"></iframe>
 					<!-- <iframe id="destination-frame" src="<?php echo get_stylesheet_directory_uri(); ?>/page-form-lightbox.php"></iframe> -->
 			</div>
 			<div class="footer"></div>
