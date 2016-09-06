1857eba8f0796af54ec569028f8ec9d6b68b3fee
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-09-06 11:15:30 -0500

    producto simple y variable con if para botón carrito

--- themes/invogue-child/woocommerce/single-product/price.php
+++ themes/invogue-child/woocommerce/single-product/price.php
@@ -27,57 +27,123 @@ if ( ! $product->is_purchasable() ) {
 }
 
 ?>
-<div class="htheme_single_product_price">
-	<?php echo $product->get_price_html(); ?>
-	<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
-	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
-	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
-</div>
 
-<!-- origin in simple -->
-<?php
-	// Availability
-	$availability      = $product->get_availability();
-	$availability_html = empty( $availability['availability'] ) ? '' : '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>';
+<?php if( $product->is_type( 'simple' ) ): ?>
 
-	echo apply_filters( 'woocommerce_stock_html', $availability_html, $availability['availability'], $product );
-?>
+	<!-- product simple -->
+	<div class="htheme_single_product_price">
+		<?php echo $product->get_price_html(); ?>
+		<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
+		<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
+		<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+	</div>
 
-<?php if ( $product->is_in_stock() ) : ?>
+	<!-- origin in simple -->
+	<?php
+		// Availability
+		$availability      = $product->get_availability();
+		$availability_html = empty( $availability['availability'] ) ? '' : '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>';
 
-	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+		echo apply_filters( 'woocommerce_stock_html', $availability_html, $availability['availability'], $product );
+	?>
 
-	<form class="cart" method="post" enctype='multipart/form-data'>
-	 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+	<?php if ( $product->is_in_stock() ) : ?>
 
-		<div class="htheme_single_product_add">
-			<?php
-			if ( ! $product->is_sold_individually() ) {
-				?>
-				<div class="htheme_single_product_add_total">
+		<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+
+		<form class="cart" method="post" enctype='multipart/form-data'>
+		 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+
+			<div class="htheme_single_product_add">
 				<?php
-				woocommerce_quantity_input( array(
-					'min_value'   => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
-					'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product ),
-					'input_value' => ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 )
-				) );
+				if ( ! $product->is_sold_individually() ) {
+					?>
+					<div class="htheme_single_product_add_total">
+					<?php
+					woocommerce_quantity_input( array(
+						'min_value'   => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
+						'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product ),
+						'input_value' => ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 )
+					) );
+					?>
+					</div>
+					<?php
+				}
 				?>
-				</div>
+
+				<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
+				<button type="submit" class="htheme_single_product_add_button"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+			</div>
+
+			<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+		</form>
+
+		<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+
+	<?php endif; ?>
+
+	<form class="variations_form cart prueba" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->id ); ?>" data-product_variations="<?php echo htmlspecialchars( json_encode( $available_variations ) ) ?>">
+		<?php do_action( 'woocommerce_before_variations_form' ); ?>
+
+		<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
+			<p class="stock out-of-stock"><?php esc_html_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
+		<?php else : ?>
+
+			<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+
+			<!-- origin in price-php -->
+			<div class="htheme_single_product_price">
+				<?php echo $product->get_price_html(); ?>
+				<meta itemprop="price" content="<?php echo esc_attr( $product->get_price() ); ?>" />
+				<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
+				<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+			</div>
+
+			<div class="single_variation_wrap [ margin-bottom--large ]">
 				<?php
-			}
-			?>
+					/**
+					 * woocommerce_before_single_variation Hook.
+					 */
+					do_action( 'woocommerce_before_single_variation' );
 
-			<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
-			<button type="submit" class="htheme_single_product_add_button"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-		</div>
+					/**
+					 * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
+					 * @since 2.4.0
+					 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
+					 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
+					 */
+					do_action( 'woocommerce_single_variation' );
 
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+					/**
+					 * woocommerce_after_single_variation Hook.
+					 */
+					do_action( 'woocommerce_after_single_variation' );
+				?>
+			</div>
+
+			<table class="variations">
+				<tbody>
+					<?php foreach ( $attributes as $attribute_name => $options ) : ?>
+						<tr>
+							<td class="label"><label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label></td>
+							<td class="value">
+								<?php
+									$selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) : $product->get_variation_default_attribute( $attribute_name );
+									wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected, 'class' => '' ) );
+									echo end( $attribute_keys ) === $attribute_name ? apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__( 'Reset', 'woocommerce' ) . '</a>' ) : '';
+								?>
+							</td>
+						</tr>
+			        <?php endforeach;?>
+				</tbody>
+			</table>
+
+			<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+		<?php endif; ?>
+
+		<?php do_action( 'woocommerce_after_variations_form' ); ?>
 	</form>
 
 	<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
 
 <?php endif; ?>
-
-
-
-
